f2f340971358f72c2f51905fb4336dec51191754
Praneesh-Sharma <pegasus30042004@gmail.com>
2026-05-19 12:35:56 +0530

    added pytest-bdd cart add automation

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -14,6 +14,9 @@ class CartPage:
     def verify_product_in_cart(self, product_name):
         return product_name in self.driver.page_source
     
+    def get_cart_total(self):
+        return self.driver.find_element(By.ID, "totalp").text
+    
     def remove_product(self):
         self.driver.find_element(By.LINK_TEXT, "Delete").click()
         time.sleep(2)
