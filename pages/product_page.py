42fe766fbc4e6096b5371dc612d5005aac4cac81
Praneesh-Sharma <pegasus30042004@gmail.com>
2026-05-21 12:21:23 +0530

    fixed issue with verify_cart

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,19 +6,22 @@ from pages.base_page import BasePage
 
 class ProductPage(BasePage):
     PRODUCT = (By.LINK_TEXT,"Samsung galaxy s6")
-
     PRODUCT_TITLE = (By.CLASS_NAME,"name")
-
     PRODUCT_PRICE = (By.CLASS_NAME,"price-container")
-
     ADD_TO_CART = (By.LINK_TEXT,"Add to cart")
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def select_product(self,product):
         self.click((By.LINK_TEXT,product))
 
     def add_product_to_cart(self):
-            
-        self.click_element(self.ADD_TO_CART)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Add to cart']"))).click()
+        alert = self.wait.until(EC.alert_is_present())
+        alert.accept()
+
     def open_website(self):
         self.driver.get("https://www.demoblaze.com")
 
