e9efde304dc9acfb1db92374844994c3afc304eb
Praneesh-Sharma <pegasus30042004@gmail.com>
2026-05-21 12:21:05 +0530

    used WebDriverWait in place of time package

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,30 +1,35 @@
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class CartPage:
     def __init__(self, driver):
         self.driver = driver
-
-    cart_button = (By.ID, "cartur")
+        self.wait = WebDriverWait(driver, 10)
 
     def open_cart(self):
-        self.driver.find_element(*self.cart_button).click()
-        time.sleep(10)
+        self.wait.until(
+            EC.element_to_be_clickable((By.ID, "cartur"))
+        ).click()
+        self.wait.until(EC.visibility_of_element_located((By.ID, "tbodyid")))
     
     def verify_product_in_cart(self, product_name):
-        return product_name in self.driver.page_source
+        product_xpath = f"//td[text()='{product_name}']"
+        try:
+            element = self.wait.until(EC.visibility_of_element_located((By.XPATH, product_xpath)))
+            return element.is_displayed()
+        except TimeoutException:
+            return False
     
     def get_cart_total(self):
-        return self.driver.find_element(By.ID, "totalp").text
-    
-    def remove_product(self):
-        self.driver.find_element(By.LINK_TEXT, "Delete").click()
-        time.sleep(10)
+        total_element = self.wait.until(EC.visibility_of_element_located((By.ID, "totalp")))
+        return total_element.text
     
     def remove_product(self, product_name):
-        delete_button_xpath = f"//td[text()='{product_name}']/following-sibling::td/a[text()='Delete']"
-        self.driver.find_element(By.XPATH, delete_button_xpath).click()
-        time.sleep(10)
+        delete_button_xpath = (f"//td[text()='{product_name}']/following-sibling::td/a[text()='Delete']")
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, delete_button_xpath))).click()
+        self.wait.until(EC.invisibility_of_element_located((By.XPATH, f"//td[text()='{product_name}']")))
     
     def is_cart_empty(self):
         products = self.driver.find_elements(By.XPATH, "//tbody[@id='tbodyid']/tr")
